8cbd98a4f3a9d6738d95aec1acf816ed2f8f4b4d
Le Ngan <lengan295@gmail.com>
2016-08-08 13:21:39 +0200

    edit welcome page 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Laravel</title>
+		<title>Le Ngan</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
@@ -10,7 +10,7 @@
 				padding: 0;
 				width: 100%;
 				height: 100%;
-				color: #B0BEC5;
+				color: #A1AFB6;
 				display: table;
 				font-weight: 100;
 				font-family: 'Lato';
